436b3b148bcb093b57e1fd12a32ebc9f3cf47ecc
Matt <matthew.j.thrasher@gmail.com>
2016-01-23 13:54:03 -0600

    Remove express.morgan from web.js file

--- web.js
+++ web.js
@@ -4,6 +4,6 @@ var gzippo = require('gzippo');
   var morgan = require('morgan');
   var app = express();
  
-  app.use(express.morgan('dev'));
+  app.use(morgan('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 5000);
